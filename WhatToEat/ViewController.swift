af322ae990fccecd1233202f44ca3c1f497a10c6
jting <jo841130s@gmail.com>
2019-11-06 13:06:46 +0800

    Initial Commit

---
+++ WhatToEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatToEat
+//
+//  Created by JTINGKAO on 2019/11/6.
+//  Copyright © 2019 JTINGKAO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
