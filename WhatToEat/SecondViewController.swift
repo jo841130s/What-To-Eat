b36f44a3c12ad25bbbd5ac9dea32de52335c19d9
jting <jo841130s@gmail.com>
2019-11-07 15:18:38 +0800

    11/7 還沒加入shareInstagram

--- WhatToEat/SecondViewController.swift
+++ WhatToEat/SecondViewController.swift
@@ -18,11 +18,11 @@ class SecondViewController:UIViewController,UIPickerViewDelegate,UIPickerViewDat
     
     let breakfastArray = ["蛋餅","漢堡","鐵板麵","蘿蔔糕","飯糰","蔥油餅","煎餃","饅頭","三明治","回去睡","稀飯","麥當勞","摩斯漢堡","星巴克","捲餅","包子","雞塊","水果","沙拉","麵包"]
     
-    let lunchAndDinnerArray = ["便當","義大利麵","咖哩","牛肉麵","炒麵","炒飯","壽司","牛排","火鍋","關東煮","丼飯","捲餅","沙拉","麥當勞","7-11","Family Mart","摩斯漢堡","漢堡王","鐵板燒","肉羹","燒臘","港式小點","披薩","熱炒","水餃","泡麵","台式小館","烤鴨","泰式","涼麵","減肥","BBQ","拉麵","燒烤"]
+    let lunchAndDinnerArray = ["便當","義大利麵","咖哩","牛肉麵","炒麵or飯","壽司","牛排","火鍋","關東煮","丼飯","捲餅","沙拉","麥當勞","7-11","Family Mart","摩斯漢堡","漢堡王","摩斯漢堡","鐵板燒","肉羹","燒臘","港式小點","披薩","熱炒","水餃","泡麵","烤鴨","泰式","涼麵","減肥","三明治","蛋包飯","拉麵","燒烤","沙拉","麵包"]
     
     let breakfastNumber = 20000
     
-    let lunchDinnerNumber = 34000
+    let lunchDinnerNumber = 36000
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -100,5 +100,10 @@ class SecondViewController:UIViewController,UIPickerViewDelegate,UIPickerViewDat
     @IBAction func homeButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func shareButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "share", sender: self)
+    }
+    
     
 }
