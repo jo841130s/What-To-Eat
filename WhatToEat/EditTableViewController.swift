0bc4b8405bc8f4f61d2583cd23bfa0e969eb492a
黃冠中 <jo@jode-MacBook-Air.local>
2019-12-17 23:59:03 +0800

    Add FoodOption class and CoreData

--- WhatToEat/EditTableViewController.swift
+++ WhatToEat/EditTableViewController.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2019 JTINGKAO. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class EditTableViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
--- WhatToEat/SecondViewController.swift
+++ WhatToEat/SecondViewController.swift
@@ -7,18 +7,21 @@
 //
 
 import UIKit
+import CoreData
 
-class SecondViewController:UIViewController,UIPickerViewDelegate,UIPickerViewDataSource{
+class SecondViewController : UIViewController,UIPickerViewDelegate,UIPickerViewDataSource{
     
+    @IBOutlet weak var arrows: UIStackView!
+    @IBOutlet weak var arrow: UIStackView!
     @IBOutlet weak var timeSegment: UISegmentedControl!
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var foodImage: UIImageView!
-    
+    @IBOutlet weak var homeButton: UIButton!
     @IBOutlet weak var foodPickerView: UIPickerView!
     
-    let breakfastArray = ["蛋餅","漢堡","鐵板麵","蘿蔔糕","飯糰","蔥油餅","煎餃","饅頭","三明治","回去睡","稀飯","麥當勞","摩斯漢堡","星巴克","捲餅","包子","雞塊","水果","沙拉","麵包"]
+    let breakfastArray = FoodOptionController().breakfastArray
     
-    let lunchAndDinnerArray = ["便當","義大利麵","咖哩","牛肉麵","炒麵or飯","壽司","牛排","火鍋","關東煮","丼飯","捲餅","沙拉","麥當勞","7-11","Family Mart","摩斯漢堡","漢堡王","摩斯漢堡","鐵板燒","肉羹","燒臘","港式小點","披薩","熱炒","水餃","泡麵","烤鴨","泰式","涼麵","減肥","三明治","蛋包飯","拉麵","燒烤","沙拉","麵包"]
+    let lunchAndDinnerArray = FoodOptionController().lunchAndDinnerArray
     
     let breakfastNumber = 20000
     
@@ -27,6 +30,10 @@ class SecondViewController:UIViewController,UIPickerViewDelegate,UIPickerViewDat
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        homeButton.layer.cornerRadius = 10.0
+        
+        arrow.transform = CGAffineTransform(scaleX: 1, y: -1)
+        arrows.transform = CGAffineTransform(scaleX: 1, y: -1)
         
         foodPickerView.delegate = self
         foodPickerView.dataSource = self
@@ -101,8 +108,9 @@ class SecondViewController:UIViewController,UIPickerViewDelegate,UIPickerViewDat
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func shareButton(_ sender: UIButton) {
-        performSegue(withIdentifier: "share", sender: self)
+    
+    @IBAction func editButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToEdit", sender: self)
     }
     
     
